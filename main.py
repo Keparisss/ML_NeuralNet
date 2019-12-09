820dde3ca138aa2d6e2138ffbf2bb6fe8ce6eb49
Keparisss <44916387+Keparisss@users.noreply.github.com>
2019-12-09 19:05:07 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+import numpy
+from keras import models
+from keras import layers, losses
+from numpy import loadtxt
+from keras import optimizers
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+from keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+model = models.Sequential()
+model.add(layers.Dense(8, activation='relu', input_shape=(33,)))
+model.add(layers.Dense(8, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+file = 'Attribute DataSet.xlsx'
+
+data = pd.read_csv("export_data_classifier1.csv")
+
+from sklearn.model_selection import train_test_split
+
+X, y = data.iloc[:,:].values, data.iloc[:, 4].values
+# df.iloc [ : ,1].values давал слишком большую ошибку???
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+metrics = ['accuracy', precision_m, recall_m]
+
+
+
+# 1
+print('\n\nbinary_crossentropy:\n')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=metrics)
+model.fit(X_train_std, y_train, epochs=5)
+
+print('\ntrain results:\n')
+loss, accuracy, precision, recall = model.evaluate(X_train_std, y_train, verbose=0)
+print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+
+print('\ntest results:\n')
+loss, accuracy, precision, recall = model.evaluate(X_test_std, y_test, verbose=0)
+print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+
+# 2
+print('\n\nRMSprop:\n')
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+              loss='mse',
+              metrics = metrics)
+model.fit(X_train_std, y_train,  batch_size=120, epochs=5)
+
+print('\ntrain results:\n')
+loss, accuracy, precision, recall = model.evaluate(X_train_std, y_train, verbose=0)
+print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+
+print('\ntest results:\n')
+loss, accuracy, precision, recall = model.evaluate(X_test_std, y_test, verbose=0)
+print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+
+
+# #  НАДО ЛИ ТАКОЕ ДЕЛАТЬ?
+#
+# from sklearn.tree import DecisionTreeClassifier
+# model = DecisionTreeClassifier()
+# model.fit(X_train_std, y_train)
+#
+# print('\ntrain results:\n')
+# loss, accuracy, precision, recall = model.evaluate(X_train_std, y_train, verbose=0)
+# print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+#
+# print('\ntest results:\n')
+# loss, accuracy, precision, recall = model.evaluate(X_test_std, y_test, verbose=0)
+# print("loss={:.2f}, accuracy={:.2f}, precision={:.2f}, recall={:.2f}".format(loss, accuracy, precision, recall))
+# старая оценка
+# loss_and_metrics = model.evaluate(X_train_std, y_train, batch_size=128)
+# for index, elem in enumerate(metrics):
+#     print("%s: %.2f%%" % (model.metrics_names[index], loss_and_metrics[index]*100))
---
+++ regression.py
@@ -0,0 +1,86 @@
+import numpy
+from keras import models, Sequential
+from keras import layers, losses
+from keras.layers import Dense
+from numpy import loadtxt
+from keras import optimizers
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+def build_model():
+  model = models.Sequential([
+    layers.Dense(64, activation='relu', input_shape=[len(data.keys())-1]),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+  ])
+
+  optimizer = optimizers.RMSprop(0.001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae', 'mse'])
+  return model
+
+data = pd.read_csv("export_data_regressor.csv")
+
+model = build_model()
+
+from sklearn.model_selection import train_test_split
+
+X = data.iloc[:, :-1].values
+y = data['APM'].values
+from sklearn.preprocessing import MinMaxScaler
+MMS = MinMaxScaler()
+X = MMS.fit_transform(X)
+y = y.reshape(-1, 1)
+y = MMS.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+
+# Compile the network :
+# model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+model.fit(X_train, y_train, epochs = 3)
+y_train_pred = model.predict(X_train)
+y_test_pred= model.predict(X_test)
+
+
+from sklearn.metrics import mean_squared_error
+print('MSE тренировка: %.3f, тестирование: %.3f' % (
+mean_squared_error(y_train, y_train_pred),
+mean_squared_error(y_test, y_test_pred)))
+
+from sklearn.metrics import r2_score
+print('R2 тренировка: %.3f, тестирование: %.3f' %(
+                        r2_score(y_train, y_train_pred),
+                        r2_score(y_test, y_test_pred)))
+
+
+print('\nМодель с 3 скрытыми слоями')
+
+NN_model = Sequential()
+
+# The Input Layer :
+NN_model.add(Dense(128, kernel_initializer='normal', input_shape=[len(data.keys())-1], activation='relu'))
+# The Hidden Layers :
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+
+# The Output Layer :
+NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
+
+# Compile the network :
+NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+NN_model.fit(X_train, y_train, epochs = 3)
+y_train_pred = NN_model.predict(X_train)
+y_test_pred= NN_model.predict(X_test)
+
+print('MSE тренировка: %.3f, тестирование: %.3f' % (
+mean_squared_error(y_train, y_train_pred),
+mean_squared_error(y_test, y_test_pred)))
+
+print('R2 тренировка: %.3f, тестирование: %.3f' %(
+                        r2_score(y_train, y_train_pred),
+                        r2_score(y_test, y_test_pred)))
